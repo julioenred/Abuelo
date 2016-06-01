3895d59800c6339b3c310dad79475d6ff45cabeb
julioenred <julioperezdecastro@gmail.com>
2016-06-01 12:49:47 +0200

    finish Index

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -3,6 +3,8 @@
     <head>
         <title>Norberto Sevilla</title>
 
+        <meta name="viewport" content="initial-scale=1">
+
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css">
         <link href="/css/style.css" rel="stylesheet" type="text/css">
@@ -13,11 +15,107 @@
 
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Norberto Sevilla</div>
-                <p class='btn btn-success'>Bootstrap</p>
+        <header id="myCarousel" class="carousel slide">
+              <!-- Wrapper for Slides -->
+              <div class="carousel-inner">
+                  <div class="item active">
+                      <!-- Set the first background image using inline CSS below. -->
+                      <div class="fill bg1"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the second background image using inline CSS below. -->
+                      <div class="fill bg2"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg3"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg4"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg5"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg6"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg7"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg8"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg9"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+                  <div class="item">
+                      <!-- Set the third background image using inline CSS below. -->
+                      <div class="fill bg10"></div>
+                      <div class="carousel-caption">
+                          
+                      </div>
+                  </div>
+              </div>
+        </header>
+
+        <img class='bg hidden-md hidden-lg' src="/img/carousel-1.jpg" alt="">
+        <img class='bg hidden-md hidden-lg' src="/img/carousel-2.jpg" alt="">
+        <img class='bg hidden-md hidden-lg' src="/img/carousel-3.jpg" alt="">
+
+        <div class="text-center">
+            <div class="perfil-avatar">
+                <img src="/img/perfil.jpg" class='img-responsive' alt="">
             </div>
+            <div class="perfil-label">
+                <p class='perfil-title'>Norberto Sevilla</p>
+                <p class='perfil-subtitle'>Pintor Extremeño</p> 
+                <a class='perfil-button' href="#" title="">Entrar</a>
+            </div>
+            
         </div>
+
+        <script type="text/javascript">
+            $( document ).ready(function() 
+            {
+              $('#myCarousel').carousel(
+              {
+                  interval: 3500, 
+                  pause: "false"
+              })
+            });
+        </script>
     </body>
 </html>
